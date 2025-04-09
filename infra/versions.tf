cfa2bfcfc7bf379c41e456e70b81c1de8a2b5512
timkrebs9 <timkrebs9@gmail.com>
2025-04-09 10:25:04 +0200

    Update azurerm provider version to >=3.93.0 in versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -5,12 +5,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.67.0"
+      version = ">=3.93.0"
     }
   }
 
   required_version = ">= 0.14"
-
-  
 }
-
