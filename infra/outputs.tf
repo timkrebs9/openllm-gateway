5726ce461797c3f1836f7b97b938f768b1a230b6
Tim Krebs <timkrebs9@gmail.com>
2025-05-01 13:50:01 +0000

    Fix output references for resource group and Kubernetes cluster in Terraform configuration

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: MPL-2.0
 
 output "resource_group_name" {
-  value = azurerm_resource_group.default.name
+  value = azurerm_resource_group.openllm-gateway.name // Correct reference
 }
 
 output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.default.name
+  value = azurerm_kubernetes_cluster.openllm-gateway.name // Correct reference
 }
 
 # output "host" {
