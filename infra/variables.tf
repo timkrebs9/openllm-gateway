1c25e45e9d06d4d58a0e98bb22858d62e53736a0
Tim Krebs <timkrebs9@gmail.com>
2025-05-01 13:28:20 +0000

    Refactor variables in Terraform configuration for PostgreSQL and API Management

--- infra/variables.tf
+++ infra/variables.tf
@@ -11,8 +11,30 @@ variable "password" {
 
 variable "subscription_id" {
   description = "Azure subscription ID"
-} 
+}
 
 variable "tenant_id" {
-  description = "Azure tenant ID"  
+  description = "Azure tenant ID"
+}
+
+variable "postgresql_server_name" {
+  description = "Name of the PostgreSQL server"
+  default     = "db"
+}
+
+variable "postgresql_server_admin" {
+  description = "PostgreSQL server admin username"
+  default     = "admin"
+}
+
+variable "useAPIM" {
+  description = "Flag to use Azure API Management to mediate the calls between the Web frontend and the backend API."
+  type        = bool
+  default     = false
+}
+
+variable "apimSKU" {
+  description = "Azure API Management SKU. Only used if useAPIM is true."
+  type       = string
+  default = "Consumption"
 }
