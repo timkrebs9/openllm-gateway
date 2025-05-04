a73f0c5a20bb31d53da5ad7024a3c0c511a31d86
Tim Krebs <timkrebs9@gmail.com>
2025-05-04 17:17:00 +0200

    Add static URL path to Flask app and update port configuration

--- src/frontend-service/app/app.py
+++ src/frontend-service/app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    # port muss mit containerPort in Deployment übereinstimmen
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
